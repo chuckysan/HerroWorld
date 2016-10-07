109fe08ccf9583fb427d61f7f79d6d3a072faa99
Charles Lai <charles.lai@trimajasa.com>
2016-10-07 15:49:29 +0800

    Test pulling info from FB authentication

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+
+  var promise = {
+  'var'  : 'texting',
+  'array': [1, 2, 3, 4]
+};
+
+//console.log(promise['array']);
+//console.log('Arrrrrrr');
+res.send(promise.var);
 });
 
 
+//more comments
+/*
 app.get('/w', function (req, res) {
   res.send('Hello Double Yuu!');
 });
 //app.use(express.static())
 
+*/
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(4000, function () {
+  console.log('Herroworld listening on port 4000!');
+  //console.log('Dir name is ' + __dirname);
 });
